2b710a726653a93c2267ef3a7da9d6f388d91d0f
Neimesh Viktoriia <valencia88vika@gmail.com>
2023-10-08 09:28:31 +0300

    Weather react app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e3797ab4c0msh30beef8f65e011dp1b273ajsn8db153df8265',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '3532619010a1fcfe112bb5bf07921bec';
